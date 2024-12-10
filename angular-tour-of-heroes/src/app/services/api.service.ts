2d391df092d848884a274434a642062be06d88cd
babyoilrig <catrin.mentzoni@yahoo.com>
2024-12-10 11:32:24 +0000

    Downgrading version of Angular and also importing HttpClient into app.config.ts and api.service.ts to stop errors

--- angular-tour-of-heroes/src/app/services/api.service.ts
+++ angular-tour-of-heroes/src/app/services/api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  // private httpClient = inject(HttpClient);
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  get<T>(url: string, options?: any): Observable<T> {
+    return this.httpClient.get<T>(url, options) as Observable<T>;
+  }
 }
