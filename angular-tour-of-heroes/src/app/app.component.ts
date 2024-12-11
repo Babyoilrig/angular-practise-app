a3bf1205510888e31c56aacd69a2785ed026f46b
babyoilrig <catrin.mentzoni@yahoo.com>
2024-12-11 12:34:20 +0000

    uninstalled and reinstalled primeng and commented out imports in scss to stop errors

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, HeaderComponent, FooterComponent ],
