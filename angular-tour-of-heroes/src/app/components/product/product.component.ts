f6ea5f9f457f209c29c47d8b6d9e920ef6cb9156
babyoilrig <catrin.mentzoni@yahoo.com>
2024-12-12 09:23:24 +0000

    Adding eventEmitter and Output to product component

--- angular-tour-of-heroes/src/app/components/product/product.component.ts
+++ angular-tour-of-heroes/src/app/components/product/product.component.ts
@@ -1,12 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../../types';
+import { Rating } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [
+    Rating, 
+    FormsModule
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-  @Input() product!: Product; 
+  @Input() product!: Product;
+  @Output() productOutput: EventEmitter<Product> = new EventEmitter<Product>();
+
+  ngOnit() {
+    this.productOutput.emit(this.product);
+  }
 }
