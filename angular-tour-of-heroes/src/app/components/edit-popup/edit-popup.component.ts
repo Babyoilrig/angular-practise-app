58fc34ddeb5fea5dd7fe700789fc06cb3e37221c
babyoilrig <catrin.mentzoni@yahoo.com>
2025-01-21 12:47:12 +0000

    Starting tp make the edit pop up component and adding imports

--- angular-tour-of-heroes/src/app/components/edit-popup/edit-popup.component.ts
+++ angular-tour-of-heroes/src/app/components/edit-popup/edit-popup.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-edit-popup',
-  imports: [],
+  imports: [DialogModule, CommonModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.scss'
 })
 export class EditPopupComponent {
-
+  @Input() display: boolean = false;
+  @Output() confirm = new EventEmitter<any>();
+  @Output() cancel = new EventEmitter<any>();
+  
 }
